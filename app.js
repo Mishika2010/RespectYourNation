17ffddf01b50baa77c52387b54f7824bc4597c7b
Mishika2010 <83953734+Mishika2010@users.noreply.github.com>
2021-08-13 20:31:33 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function hoist(){
+    document.getElementById("myAudio").play();
+      document.getElementById('flag').style.display = "block";
+    document.getElementById('button').style.display = "none";
+    document.getElementById('wish').style.display = "block";
+  }
+  
